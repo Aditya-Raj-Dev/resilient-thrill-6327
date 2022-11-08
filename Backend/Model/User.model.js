23c79e2fb325ab547187bbfb19e8b5963df61b8e
Aditya Raj <aadiyadav56837492@gmail.com>
2022-11-09 00:38:43 +0530

    initial-backend

---
+++ Backend/Model/User.model.js
@@ -0,0 +1,12 @@
+const mongoose=require("mongoose")
+
+const UserSchema= mongoose.Schema({
+    name:{type:String, required:true},
+    email:{type:String,required:true},
+    password:{type:String,required:true}
+})
+
+
+const UserModel=mongoose.model("user",UserSchema)
+
+module.exports={UserModel}
---
+++ Backend/Routes/Cart.route.js
@@ -0,0 +1,14 @@
+const {Router}=require("express")
+const {authentication}=require("../Auth/Auth")
+const { GetCart, Postcart, Deletecart } = require("../Controller/cart.controller")
+
+const CartRouter=Router()
+
+
+CartRouter.get("/",authentication, GetCart)
+CartRouter.post("/",authentication, Postcart)
+CartRouter.delete("/:_id",authentication,Deletecart)
+
+
+
+module.exports={CartRouter}
