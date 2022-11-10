8648d53f2857b1ebc990f14fa74ec9bdcc9ef8fe
Aditya Raj <aadiyadav56837492@gmail.com>
2022-11-10 23:08:10 +0530

    changed

---
+++ Frontend/pharmeasy/src/Aadicomp/cartpage/Cart.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import "./cart.css"
+const Cart = () => {
+  return (
+    <div>
+        <div>
+
+        </div>
+        <div>
+            
+        </div>
+    </div>
+  )
+}
+
+export default Cart
